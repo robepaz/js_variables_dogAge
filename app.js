ad76fc73089c9eb1fbbd08767b901c73c6d9dd10
Roberto Paz <fsjedi@outlook.com>
2021-09-16 09:26:32 -0400

    calculamos edad perro

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 // introducimos mi edad como constante
-const myAge = 13;
+const myAge = 13
 
 //
-let earlyYears = 2;
+let earlyYears = 2
 
-earlyYears *= 10.5;
+earlyYears *= 10.5
 
-let laterYears = myAge - 2;
+let laterYears = myAge - 2
 
 //
-laterYears *= 4; 
+laterYears *= 4
+
+console.log(earlyYears)
+console.log(laterYears)
+
+let myAgeinDogYears = earlyYears + laterYears
+
+//
+myName = 'Rob Paz'.toLowerCase()
